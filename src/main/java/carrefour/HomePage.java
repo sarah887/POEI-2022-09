64820d630cf76afa01b62770daae0d8a1df7de0b
sarah <sarah.chebab8@gmail.com>
2022-09-09 14:22:39 +0200

    added folder amazon, created carrefour tp and home page

---
+++ src/main/java/carrefour/HomePage.java
@@ -0,0 +1,12 @@
+package carrefour;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/test/java/TpCarrefour.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TpCarrefour {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.carrefour.fr/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCarrefour() {
+
+    }
+}
